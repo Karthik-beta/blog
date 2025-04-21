f816007de107f32621e4d5c380e79511b9979c88
Karthik-beta <karthikpar@protonmail.com>
2025-04-21 08:04:47 +0530

    0.0.1

--- components/BlogMeta.tsx
+++ components/BlogMeta.tsx
@@ -9,13 +9,13 @@ export default function BlogMeta() {
       <link
         rel="apple-touch-icon"
         sizes="180x180"
-        href="/favicon/apple-touch-icon.png"
+        href="/favicon/P_White.svg"
       />
       <link
         rel="icon"
         type="image/png"
         sizes="32x32"
-        href="/favicon/favicon-32x32.png"
+        href="/favicon/P_White.svg"
       />
       <link
         rel="icon"
@@ -24,7 +24,7 @@ export default function BlogMeta() {
         href="/favicon/favicon-16x16.png"
       />
       <link rel="manifest" href="/favicon/site.webmanifest" />
-      <link rel="shortcut icon" href="/favicon/favicon.ico" />
+      <link rel="shortcut icon" href="/favicon/P_White.svg" />
       <meta name="msapplication-TileColor" content="#000000" />
       <meta name="msapplication-config" content="/favicon/browserconfig.xml" />
       <meta name="theme-color" content="#000" />
