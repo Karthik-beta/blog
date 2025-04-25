660bc1d5fc1bdd6d4dad6a04e6a763f956ddcde4
Karthik-beta <karthikpar@protonmail.com>
2025-04-25 16:57:09 +0530

    0.0.1

--- components/BlogLayout.tsx
+++ components/BlogLayout.tsx
@@ -14,6 +14,19 @@ export default function BlogLayout({
       <div className="min-h-screen">
         <AlertBanner preview={preview} loading={loading} />
         <main>{children}</main>
+        <footer className="py-6">
+          <div className="container mx-auto text-center">
+            <p>&copy; 2024 - {new Date().getFullYear()},{" "} 
+            <a
+            rel="noreferrer noopener"
+            target="_blank"
+            href="https://www.pivotr.in"
+            className="text-primary transition-all border-primary hover:border-b-2"
+          >
+            Pivotr Technologies Pvt Ltd.{" "}
+          </a>All rights reserved.</p>
+          </div>
+        </footer>
       </div>
     </>
   )
